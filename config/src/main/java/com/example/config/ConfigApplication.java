f9abc8a6f440de2946c460fc1f09baba46b1cd1b
JackHe15 <870138846@qq.com>
2019-07-05 00:30:46 +0800

    优化注册中心

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -2,24 +2,17 @@ package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.config.server.environment.NativeEnvironmentProperties;
-import org.springframework.cloud.config.server.environment.NativeEnvironmentRepository;
-import org.springframework.cloud.config.server.environment.NativeEnvironmentRepositoryFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient(autoRegister = false)
+//@EnableDiscoveryClient(autoRegister = true)
+@EnableEurekaClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
-		//PropertySourceLocator
-		//@Retryable
-		ConfigServicePropertySourceLocator
 		SpringApplication.run(ConfigApplication.class, args);
 	}
 
@@ -29,5 +22,4 @@ public class ConfigApplication {
 //			NativeEnvironmentProperties environmentProperties) {
 //		return factory.build(environmentProperties);
 //	}
-
 }
